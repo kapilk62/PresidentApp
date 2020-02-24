cf543fdd338ed4253ae9147731af3abf9bccc872
yashvarde55 <40386161+yashvarde55@users.noreply.github.com>
2020-02-24 16:44:36 +0530

    sidemenuyash

--- app/src/main/java/com/example/presidentapp/MainActivity.java
+++ app/src/main/java/com/example/presidentapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.presidentapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerlayout ;
+    private ActionBarDrawerToggle mToggle ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle =  new ActionBarDrawerToggle(this, mDrawerlayout,R.string.open,R.string.close) ;
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
