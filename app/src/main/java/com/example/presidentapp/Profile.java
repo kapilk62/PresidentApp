8f7955a84311bf6de7644340288febafedbacb72
yashvarde55 <40386161+yashvarde55@users.noreply.github.com>
2020-03-06 17:09:39 +0530

    Profile Done

--- app/src/main/java/com/example/presidentapp/Profile.java
+++ app/src/main/java/com/example/presidentapp/Profile.java
@@ -3,6 +3,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +17,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 
 public class Profile extends AppCompatActivity{
     private static final String TAG = "ProfileActivity";
-    TextInputEditText profilFirsName,profilLastName,profileEmail,profileMobile;
+    TextInputEditText profileFirstName,profileLastName,profileEmail,profileMobile;
     Button updateprofilebutton;
     String FIRST_NAME = null;
     String LAST_NAME = null;
@@ -34,10 +37,10 @@ public class Profile extends AppCompatActivity{
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile1);
-        profilFirsName = findViewById(R.id.firstname_txt_fld);
-        profilLastName = findViewById(R.id.lastename_txt_fld);
+        profileFirstName = findViewById(R.id.firstname_txt_fld);
+        profileLastName = findViewById(R.id.lastename_txt_fld);
         profileEmail = findViewById(R.id.email_txt_fld);
-        profileMobile = findViewById(R.id.mob_no_txt_fld);
+        profileMobile = (TextInputEditText) findViewById(R.id.mob_no_txt_fld);
         updateprofilebutton = findViewById(R.id.updateprofile_btn);
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -48,8 +51,8 @@ public class Profile extends AppCompatActivity{
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
-                profilFirsName.setText(user.getFirst_name());
-                profilLastName.setText(user.getLast_name());
+                profileFirstName.setText(user.getFirst_name());
+                profileLastName.setText(user.getLast_name());
                 profileEmail.setText(user.getEmail());
                 profileMobile.setText(user.getMobile_number());
 
@@ -57,10 +60,45 @@ public class Profile extends AppCompatActivity{
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Profile.this, databaseError.getCode(),Toast.LENGTH_LONG).show();
+            }
+        });
 
+        updateprofilebutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String first_name = profileFirstName.getText().toString().trim();
+                String last_name = profileLastName.getText().toString().trim();
+                String email = profileEmail.getText().toString().trim();
+                String mobile_number = profileMobile.getText().toString();
+                Log.d(TAG, "onClick: "+mobile_number);
+                if(TextUtils.isEmpty(first_name)){
+                    profileFirstName.setError("First Name Required");
+                }
+                if(TextUtils.isEmpty(last_name)){
+                    profileLastName.setError("Last Name Required");
+                }
+                if(TextUtils.isEmpty(email)){
+                    profileEmail.setError("Email Id Required");
+                }
+                if(TextUtils.isEmpty(mobile_number)){
+                    profileMobile.setError("Mobile Number Required");
+                }
+                updateprofile(first_name,last_name,email,mobile_number);
+                finish();
             }
         });
 
     }
 
+    private boolean updateprofile(String FirstName, String LastName, String Email , String MobileNumber){
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseAuth.getUid());
+        User user = new User(FirstName, LastName, Email, MobileNumber);
+        databaseReference.setValue(user);
+        Toast.makeText(this, "Profile Updated", Toast.LENGTH_LONG).show();
+        return true;
+    }
 }
+
+
