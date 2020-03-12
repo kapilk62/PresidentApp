b8e7be8c46b4a86c72d9284e8466b78f2ab3cda9
Rathod <vgrathod46@gmail.com>
2020-03-12 16:59:10 +0530

    emergency number done by vishal

--- app/src/main/java/com/example/presidentapp/EmergencyFragment.java
+++ app/src/main/java/com/example/presidentapp/EmergencyFragment.java
@@ -2,16 +2,27 @@ package com.example.presidentapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.presidentapp.Adapter.Emergency_number_adapter;
+import com.example.presidentapp.Model.Emergency_Num_Model;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -24,25 +35,51 @@ public class EmergencyFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    ListView listViewEmergencyNumbers;
+    DatabaseReference databaseEmergencyNumbers;
+    List<Emergency_Num_Model> EmergencyNumberList;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    Activity context;
+    private Activity context;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_emergency, container, false);
         context=getActivity();
+        databaseEmergencyNumbers = FirebaseDatabase.getInstance().getReference("Emergency_Numbers");
+        listViewEmergencyNumbers = (ListView) v.findViewById(R.id.listViewEmergencyNumber);
+        EmergencyNumberList = new ArrayList<>();
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency, container, false);
+        return v;
+
     }
 
     public void onStart(){
         super.onStart();
-        FloatingActionButton bt=(FloatingActionButton) context.findViewById(R.id.Emergency_floatingActionButton);
+        databaseEmergencyNumbers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                EmergencyNumberList.clear();
+                for(DataSnapshot emergencyNumberSnapshot : dataSnapshot.getChildren()) {
+                    Emergency_Num_Model emergency_num_model = emergencyNumberSnapshot.getValue(Emergency_Num_Model.class);
+                    EmergencyNumberList.add(emergency_num_model);
+                }
+                Emergency_number_adapter emergency_number_adapter = new Emergency_number_adapter(getActivity(),EmergencyNumberList);
+                listViewEmergencyNumbers.setAdapter(emergency_number_adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        FloatingActionButton bt= context.findViewById(R.id.Emergency_floatingActionButton);
         bt.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                
+
                 //create an Intent object
                 Intent intent=new Intent(context, EmergencyForm.class);
                 //add data to the Intent object
@@ -82,10 +119,5 @@ public class EmergencyFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
-
-
-
-
 }
--- app/src/main/java/com/example/presidentapp/Model/Emergency_Num_Model.java
+++ app/src/main/java/com/example/presidentapp/Model/Emergency_Num_Model.java
@@ -1,4 +1,28 @@
 package com.example.presidentapp.Model;
 
 public class Emergency_Num_Model {
+    String emergencyNumberId;
+    String emergencyName;
+    String emergencyNumber;
+
+    public Emergency_Num_Model () {}
+
+    public Emergency_Num_Model(String emergencyNumberId, String emergencyName, String emergencyNumber) {
+        this.emergencyNumberId = emergencyNumberId;
+        this.emergencyName = emergencyName;
+        this.emergencyNumber = emergencyNumber;
+    }
+
+    public String getEmergencyNumberId() {
+        return emergencyNumberId;
+    }
+
+    public String getEmergencyName() {
+        return emergencyName;
+    }
+
+    public String getEmergencyNumber() {
+        return emergencyNumber;
+    }
 }
+
