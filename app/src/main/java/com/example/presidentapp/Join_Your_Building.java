ca3e982c50c3f3fcc7a353bf4d50dc6963023992
yashvarde55 <40386161+yashvarde55@users.noreply.github.com>
2020-03-16 13:13:01 +0530

    finnallly new building

---
+++ app/src/main/java/com/example/presidentapp/Join_Your_Building.java
@@ -0,0 +1,14 @@
+package com.example.presidentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Join_Your_Building extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join__your__building);
+    }
+}
