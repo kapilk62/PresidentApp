70795df8b39bf64f3911972741e0557dbf76ef8e
Rathod <vgrathod46@gmail.com>
2020-03-17 16:17:48 +0530

    documents complete by vishal

--- app/src/main/java/com/example/presidentapp/MyBuilding/MyBuildingDocuments.java
+++ app/src/main/java/com/example/presidentapp/MyBuilding/MyBuildingDocuments.java
@@ -1,16 +1,118 @@
 package com.example.presidentapp.MyBuilding;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.example.presidentapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 
-public class MyBuildingDocuments extends AppCompatActivity {
+public class MyBuildingDocuments extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int PICK_IMAGE_REQUEST = 123;
+    private Button btnChoose, btnUpload;
+    private ImageView docImageView;
+    private Uri filepath;
+
+    private StorageReference mStorageRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_building_documents);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        docImageView = findViewById(R.id.documentsImageView);
+        btnChoose = findViewById(R.id.btn_choose);
+        btnUpload = findViewById(R.id.btn_upload);
+
+        btnChoose.setOnClickListener(this);
+        btnUpload.setOnClickListener(this);
+    }
+
+
+    private void uploadFile() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading...");
+        progressDialog.show();
+        if (filepath != null) {
+            StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+            riversRef.putFile(filepath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    progressDialog.setMessage(((int) progress) + "% Uploading...");
+                }
+            });
+        } else {
+            //display a error toast
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select an Image"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filepath = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                docImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnChoose) {
+            //open file chooser
+            showFileChooser();
+        } else if (view == btnUpload) {
+            //upload file to fire base
+            uploadFile();
+        }
     }
 }
