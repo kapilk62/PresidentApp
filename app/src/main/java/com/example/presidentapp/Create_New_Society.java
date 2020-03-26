b5966c8a2cc5ec0c314eec644cdaa85063d66141
Rathod <vgrathod46@gmail.com>
2020-03-26 20:22:44 +0530

    multiple building page working by vishal

--- app/src/main/java/com/example/presidentapp/Create_New_Society.java
+++ app/src/main/java/com/example/presidentapp/Create_New_Society.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
---
+++ app/src/main/java/com/example/presidentapp/multiple_building_page.java
@@ -0,0 +1,62 @@
+package com.example.presidentapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.presidentapp.Model.CreateNewSocietyModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class multiple_building_page extends AppCompatActivity {
+
+    private RecyclerView adminBuildingList;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_building_page);
+        String currentuserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(currentuserId).child("New Building");
+        databaseReference.keepSynced(true);
+
+        adminBuildingList = findViewById(R.id.recyclerviewadmin);
+        adminBuildingList.setHasFixedSize(true);
+        adminBuildingList.setLayoutManager( new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<CreateNewSocietyModel,buildingAdminViewHolder>firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<CreateNewSocietyModel, buildingAdminViewHolder>
+                (CreateNewSocietyModel.class,R.layout.cardview_admin,buildingAdminViewHolder.class,databaseReference) {
+            @Override
+            protected void populateViewHolder(buildingAdminViewHolder buildingAdminViewHolder, CreateNewSocietyModel createNewSocietyModel, int i) {
+                buildingAdminViewHolder.setBuildingName(createNewSocietyModel.getBuildingName());
+            }
+        };
+        adminBuildingList.setAdapter(firebaseRecyclerAdapter);
+    }
+    public static class buildingAdminViewHolder extends RecyclerView.ViewHolder{
+        View view;
+        public buildingAdminViewHolder(View itemView)
+        {
+            super(itemView);
+            view=itemView;
+        }
+        public void setBuildingName(String buildingName){
+            TextView building_name = view.findViewById(R.id.building_name_cardView);
+            building_name.setText(buildingName);
+        }
+    }
+}
